82516f3c002a05b1e4e3fa201340f1327bb10bc8
Robert Reilley <rpreilley@gmail.com>
2016-07-06 10:05:37 -0500

    pulling partners code after this commit

--- models/profile.js
+++ models/profile.js
@@ -1,13 +1,10 @@
 var mongoose = require('mongoose');
 
 var profileSchema = new mongoose.Schema({
-  user_id: { type: Number, required: true },
   location: String,
   aboutme: String,
-  age: Number,
-  preference: String,
-  post_id: Number
+  age: Number
 })
 
 var Profile = mongoose.model('Profile', profileSchema);
-module.exports = Profile;
+mongoose.model('profiles', profileSchema);
