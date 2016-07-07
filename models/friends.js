33dc4b3e370e3309f477c6a2d26fb85c52ec574c
Zach Gaines <zrgaines@mtu.edu>
2016-07-07 09:24:53 -0500

    some more css

--- models/friends.js
+++ models/friends.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var friendSchema = new mongoose.Schema({
   created_at: {type: Date, required: true, default: Date.now},
-
+  
 })
 
 var Profile = mongoose.model('Profile', profileSchema);
